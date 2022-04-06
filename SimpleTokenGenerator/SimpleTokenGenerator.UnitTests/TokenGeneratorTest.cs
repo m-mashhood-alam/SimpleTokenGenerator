fa4d36f4deab541cb1899cf60c6125bf6eb29f2b
m-m-alam <m.mashhood.alam@outlook.com>
2022-04-06 07:18:39 +0500

    first commit

---
+++ SimpleTokenGenerator/SimpleTokenGenerator.UnitTests/TokenGeneratorTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SimpleTokenGenerator.UnitTests
+{   [TestClass]
+    public class TokenGeneratorTest
+    {
+        [TestMethod]
+        public void GetTokenSignature()
+        {
+            var token = SimpleTokenGenerator.GetToken();
+            var currentTimeStamp = DateTime.Now.ToOADate();
+            var sampleSecret = Guid.Empty.ToString();
+
+            var tokenSignature = SimpleTokenGenerator.GetTokenSignature(currentTimeStamp, token, sampleSecret);
+
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ SimpleTokenGenerator/SimpleTokenGenerator/SimpleTokenGenerator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleTokenGenerator
+{
+    public class SimpleTokenGenerator
+    {
+        /// <summary>
+        ///  This function returns a signature[encrypted token] based upon provided timestamp, token and a secret key that is shared between the sender and a receiver.
+        /// </summary>
+        /// <param name="timeStamp"></param>
+        /// <param name="token"></param>
+        /// <param name="secret"></param>
+        /// <returns></returns>
+        public static string GetTokenSignature(double timeStamp, string token, string secret)
+        {
+            var encryptedToken = GetHash(timeStamp.ToString() + token, secret);
+            return encryptedToken;
+        }
+
+        /// <summary>
+        ///  Returns a random string of length 50
+        /// </summary>
+        /// <returns></returns>
+        public static string GetToken()
+        {
+            var plainToken = GetRandomString();
+            return plainToken;
+        }
+
+        #region helper methods
+        private static string GetRandomString()
+        {
+            var allChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            var resultString = new string(
+               Enumerable.Repeat(allChars, 50)
+               .Select(str => str[random.Next(str.Length)]).ToArray());
+
+            return resultString;
+        }
+        private static string GetHash(string userString, string signingKey)
+        {
+            var encoding = new UTF8Encoding();
+
+            var textBytes = encoding.GetBytes(userString);
+            var keyBytes = encoding.GetBytes(signingKey);
+
+            byte[] hashBytes;
+
+            using (var hash = new HMACSHA256(keyBytes))
+                hashBytes = hash.ComputeHash(textBytes);
+
+            return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        }
+        #endregion
+    }
+}
